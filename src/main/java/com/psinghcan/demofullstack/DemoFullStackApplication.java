19d3ee6eb7d54609a01df20de3e063edbf8ba4fb
psinghcan <psinghcan@gmail.com>
2021-07-29 23:27:03 -0400

    init

---
+++ src/main/java/com/psinghcan/demofullstack/DemoFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.psinghcan.demofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psinghcan/demofullstack/DemoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psinghcan.demofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
